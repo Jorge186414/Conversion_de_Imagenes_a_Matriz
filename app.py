cbb46cec6c1d0954a3658d5b0e788ee742321f1d
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-16 18:25:58 -0600

    MODIFIED: Eliminacion de mensajes de verificacion

--- app.py
+++ app.py
@@ -41,14 +41,14 @@ def cargar_imagen():
         imagen_blanco_negro_path = os.path.join(app.config['imagenes'], 'bn_' + archivo.filename)
         imagen_blanco_negro.save(imagen_blanco_negro_path)
 
-        # Mostrar matriz en consola
         matriz_blanco_negro = np.array(imagen_blanco_negro)
-        print(matriz_blanco_negro)
 
-        # Devolver las rutas de las imágenes como JSON
+        matriz_blanco_negro = matriz_blanco_negro.tolist()
+
         return jsonify({
             'Imagen_Original': archivo.filename,
-            'Imagen_Blanco_Negro': 'bn_' + archivo.filename
+            'Imagen_Blanco_Negro': 'bn_' + archivo.filename,
+            'Matriz_de_Imagen': matriz_blanco_negro
         })
 
 @app.route('/uploads/<filename>')
