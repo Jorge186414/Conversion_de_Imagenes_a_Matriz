26df79f30bcfc4b851dfe8099e53bfbe4e8831d6
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-09-16 19:41:40 -0600

    UPDATE: Formateo de codigo en app.py

--- app.py
+++ app.py
@@ -14,42 +14,42 @@ os.makedirs(imagenes, exist_ok=True)
 
 @app.route('/')
 def index():
-    # Usar render_template en lugar de send_static_file
-    return render_template('index.html')
+  # Usar render_template en lugar de send_static_file
+  return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def cargar_imagen():
-    if 'archivo' not in request.files:
-        return jsonify({'error': 'No se encontró el archivo'})
-    
-    archivo = request.files['archivo']
-    
-    if archivo.filename == '':
-        return jsonify({'error': 'No se cargó ninguna imagen'})
-    
-    if archivo:
-        ruta_imagen = os.path.join(app.config['imagenes'], archivo.filename)
-        archivo.save(ruta_imagen)
+  if 'archivo' not in request.files:
+    return jsonify({'error': 'No se encontró el archivo'})
+  
+  archivo = request.files['archivo']
+  
+  if archivo.filename == '':
+    return jsonify({'error': 'No se cargó ninguna imagen'})
+  
+  if archivo:
+    ruta_imagen = os.path.join(app.config['imagenes'], archivo.filename)
+    archivo.save(ruta_imagen)
 
-        # Cargar la imagen
-        imagen_original = Image.open(ruta_imagen)
+    # Cargar la imagen
+    imagen_original = Image.open(ruta_imagen)
 
-        # Convertir a blanco y negro
-        imagen_blanco_negro = imagen_original.convert('L')
+    # Convertir a blanco y negro
+    imagen_blanco_negro = imagen_original.convert('L')
 
-        # Guardar la imagen en blanco y negro
-        imagen_blanco_negro_path = os.path.join(app.config['imagenes'], 'bn_' + archivo.filename)
-        imagen_blanco_negro.save(imagen_blanco_negro_path)
+    # Guardar la imagen en blanco y negro
+    imagen_blanco_negro_path = os.path.join(app.config['imagenes'], 'bn_' + archivo.filename)
+    imagen_blanco_negro.save(imagen_blanco_negro_path)
 
-        matriz_blanco_negro = np.array(imagen_blanco_negro)
+    matriz_blanco_negro = np.array(imagen_blanco_negro)
 
-        matriz_blanco_negro = matriz_blanco_negro.tolist()
+    matriz_blanco_negro = matriz_blanco_negro.tolist()
 
-        return jsonify({
-            'Imagen_Original': archivo.filename,
-            'Imagen_Blanco_Negro': 'bn_' + archivo.filename,
-            'Matriz_de_Imagen': matriz_blanco_negro
-        })
+    return jsonify({
+        'Imagen_Original': archivo.filename,
+        'Imagen_Blanco_Negro': 'bn_' + archivo.filename,
+        'Matriz_de_Imagen': matriz_blanco_negro
+    })
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
